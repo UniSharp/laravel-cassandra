e30a00fdee6506d85d8f6f50c0b9a96cbc23cad8
FreedomKnight <fk@unisharp.com>
2017-07-06 15:58:13 +0800

    add all filtering

--- src/Query/Builder.php
+++ src/Query/Builder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Builder extends QueryBuilder
 {
+    public $allows = [];
     public function delete($id = null)
     {
         if (!is_null($id) && !is_null($this->from)) {
@@ -16,5 +17,11 @@ class Builder extends QueryBuilder
             $this->grammar->compileDelete($this),
             $this->getBindings()
         );
+    }
+
+    public function allowFiltering()
+    {
+        $this->allows[] = 'filtering';
+        return $this;
     }
 }
--- src/Query/Grammars/Grammar.php
+++ src/Query/Grammars/Grammar.php
@@ -2,8 +2,32 @@
 namespace Unisharp\Cassandra\Query\Grammars;
 
 use \Illuminate\Database\Query\Grammars\Grammar as BaseGrammar;
+use Unisharp\Cassandra\Query\Builder;
 
 class Grammar extends BaseGrammar
 {
+    protected $selectComponents = [
+        'aggregate',
+        'columns',
+        'from',
+        'joins',
+        'wheres',
+        'groups',
+        'havings',
+        'orders',
+        'limit',
+        'offset',
+        'unions',
+        'lock',
+        'allows'
+    ];
 
+    public function compileAllows(Builder $builder, array $allows)
+    {
+        if (count($allows) > 0) {
+            return 'allow '. implode(' ', $allows);
+        }
+
+        return '';
+    }
 }
